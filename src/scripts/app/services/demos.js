78cc42dbf2632fced21fc6b063bff459673b558c
yjeffrey <jeff_8514@hotmail.com>
2014-12-12 21:02:37 -0800

    Included files this time

---
+++ src/scripts/app/services/demos.js
@@ -0,0 +1,22 @@
+define(['./main'],
+	function (servicesModule) {
+
+		servicesModule.constant('demos', [
+			'base.demo.sudoku', 
+			'base.demo.quadrature',
+			'base.demo.modulo',
+			'base.demo.wave',
+			'base.demo.pi'
+		]);
+		
+		servicesModule.factory('DemoService', ['demos', function(demos) {
+		
+			return {
+				demos: demos,
+				getRandomDemo: function(){
+					var demos = this.demos;
+					return demos[Math.floor(Math.random() * demos.length)];
+				}
+			};
+		}]);
+	});
